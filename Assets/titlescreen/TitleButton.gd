1c85b9327d0d6ddc2165b7c89ee81decf0e18bf7
HarrisonNorth0816 <hanorth@iu.edu>
2023-12-09 21:00:21 -0500

    update

--- Assets/titlescreen/TitleButton.gd
+++ Assets/titlescreen/TitleButton.gd
@@ -1,4 +1,11 @@
-extends Button
+extends Node2D
 
-func _on_pressed():
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://World/game.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
+
+
+
--- Global.gd
+++ Global.gd
@@ -2,10 +2,26 @@ extends Node
 
 var kills = 0 
 
-
 func _ready():
-	process_mode = PROCESS_MODE_ALWAYS		# global should never be paused
+	process_mode = Node.PROCESS_MODE_ALWAYS		# global should never be paused
 	
+	
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		var menu = get_node("/root/Game/UI/Menu")
+		if menu == null:
+			get_tree().quit()
+		else:
+			if menu.hidden:
+				print("you can see the menu")
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+				get_tree().paused = true
+				menu.show()
+			else:
+				print("You cant see the menu")
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+				get_tree().paused = false
+				menu.hide()
 
 func update_deaths():
 	kills += 1
