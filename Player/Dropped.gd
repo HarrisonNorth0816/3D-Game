a4014a3596dd811bd4d5370290b110e2f3b9912a
HarrisonNorth0816 <hanorth@iu.edu>
2023-12-10 11:04:32 -0500

    update

--- Player/Dropped.gd
+++ Player/Dropped.gd
@@ -1,1 +1,10 @@
 extends RigidBody3D
+
+
+@onready var Weapon = load("res://Player/shotgun.tscn")
+
+func _on_area_3d_body_entered(body):
+	for b in $Area3D.get_overlapping_bodies():
+		var weapon = Weapon.instantiate()
+		b.pickup(weapon)
+		queue_free()
