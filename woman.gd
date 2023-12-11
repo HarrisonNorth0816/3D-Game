cbb9610723ef41c1db8927f162ced2d3ec8cad2b
HarrisonNorth0816 <hanorth@iu.edu>
2023-12-10 20:30:30 -0500

    github

---
+++ woman.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+func _on_area_3d_body_entered(body):
+	$Read.show()
+	$Read/Control/Label.text = "A hole opened into the catacombs, they are shifting around constantly,
+	there was some kind of demon ritual down there.
+	Father Garcia was lost down there!
+	Use that shotgun of yours to help us Father!"
+	$Timer.start()
+
+func _on_timer_timeout():
+	$Read.hide()
